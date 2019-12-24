8f8dc5fcbf619704d28045b4fe04153114637402
Vignesh <33924596+SomeRandomGuyOnTheInternet@users.noreply.github.com>
2019-12-24 17:48:50 +0530

    Changed the user class a bit

--- MyCircles/BLL/User.cs
+++ MyCircles/BLL/User.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 
 namespace MyCircles.BLL
 {
     public class User
     {
         public string Username { get; set; }
-        public string EmailAddr { get; set; }
+        public string EmailAddress { get; set; }
         public string Name { get; set; }
         public string Bio { get; set; }
         public decimal xGeoPos { get; set; }
@@ -17,18 +18,24 @@ namespace MyCircles.BLL
         public string HeaderImage { get; set; }
         public bool isLoggedIn { get; set; }
         public bool IsPrivileged { get; set; }
-
-        public User(string Username, string EmailAddr, string Name)
+        public string Password
         {
-            this.Username = Username;
-            this.EmailAddr = EmailAddr;
-            this.Name = Name;
-            this.Bio = "";
+            get { return Password; }
+            set { Password = Crypto.HashPassword(value); }
+        }
+
+        public User(string username, string emailAddress, string name, string password)
+        {
+            this.Username = username;
+            this.EmailAddress = emailAddress;
+            this.Name = name;
+            this.Password = password;
+            this.Bio = null;
             this.xGeoPos = 0;
             this.yGeoPos = 0;
-            this.ProfileImage = "";
-            this.HeaderImage = "";
-            this.isLoggedIn = true;
+            this.ProfileImage = null;
+            this.HeaderImage = null;
+            this.isLoggedIn = false;
             this.IsPrivileged = false;
         }
     }
