5f56152ba6b2e2abc57b51793392a0b589c21b35
Lam En Qing <183129H@mymail.nyp.edu.sg>
2019-12-24 18:43:50 +0800

    Created User for BLL and DAL

---
+++ MyCircles/BLL/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyCircles.BLL
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string EmailAddr { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public decimal xGeoPos { get; set; }
+        public decimal yGeoPos { get; set; }
+        public string ProfileImage { get; set; }
+        public string HeaderImage { get; set; }
+        public bool isLoggedIn { get; set; }
+        public bool IsPrivileged { get; set; }
+
+        public User(string Username, string EmailAddr, string Name)
+        {
+            this.Username = Username;
+            this.EmailAddr = EmailAddr;
+            this.Name = Name;
+            this.Bio = "";
+            this.xGeoPos = 0;
+            this.yGeoPos = 0;
+            this.ProfileImage = "";
+            this.HeaderImage = "";
+            this.isLoggedIn = true;
+            this.IsPrivileged = false;
+        }
+    }
+}
