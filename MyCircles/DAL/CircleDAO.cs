b84cefffa250af6c570b25642549ce3cf6a38e85
Vignesh <33924596+SomeRandomGuyOnTheInternet@users.noreply.github.com>
2020-01-14 23:10:12 +0800

    commit to merge

---
+++ MyCircles/DAL/CircleDAO.cs
@@ -0,0 +1,26 @@
+﻿using MyCircles.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyCircles.DAL
+{
+    public static class CircleDAO
+    {
+        public static void AddCircle(string circleName)
+        {
+            using (var db = new MyCirclesEntityModel())
+            {
+                Circle newCircle = new Circle();
+                Circle existingCircle = db.Circles.Where(ec => ec.Name == circleName).FirstOrDefault();
+
+                if (existingCircle == null)
+                {
+                    db.Circles.Add(newCircle);
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ MyCircles/DAL/UserCircleDAO.cs
@@ -0,0 +1,28 @@
+﻿using MyCircles.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyCircles.DAL
+{
+    public static class UserCircleDAO
+    {
+        public static void AddUserCircle(int userId, string circleName)
+        {
+            using (var db = new MyCirclesEntityModel())
+            {
+                Circle newCircle = new Circle();
+                Circle existingCircle = db.Circles.Where(ec => ec.Name == circleName).FirstOrDefault();
+
+                if (existingCircle == null)
+                {
+
+
+                    db.Circles.Add(newCircle);
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
