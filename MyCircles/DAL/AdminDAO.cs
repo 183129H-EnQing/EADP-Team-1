165f69f92665f96932e677b8e88d930e9616cb4d
Lam En Qing <183129h@mymail.nyp.edu.sg>
2020-01-15 01:14:59 +0800

    Created admindao, but no admin inside BLL, later do

---
+++ MyCircles/DAL/AdminDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyCircles.DAL
+{
+    public class AdminDAO
+    {
+        //public static void AddAdmin(Admin admin)
+        //{
+
+        //}
+    }
+}
