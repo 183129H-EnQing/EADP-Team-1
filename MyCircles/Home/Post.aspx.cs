7e28c1f054e1a7d5e072fd2b26b1430a678ab6b3
Vignesh <33924596+SomeRandomGuyOnTheInternet@users.noreply.github.com>
2020-02-05 09:42:53 +0800

    Update Post.aspx.cs

--- MyCircles/Home/Post.aspx.cs
+++ MyCircles/Home/Post.aspx.cs
@@ -47,15 +47,14 @@ namespace MyCircles.Home
 
         protected void btnPost_Click(object sender, EventArgs e)
         {
-          
-                try
+            try
             {
                 var newPost = new BLL.Post();
                 newPost.Content = activity.Text;
                 newPost.DateTime = DateTime.Now;
                 newPost.UserId = currentUser.Id;
                 newPost.CircleId = "gym";
-                newPost.Image = UploadThisFile(FileUpload1);
+                newPost.Image = GeneralHelpers.UploadFile(FileUpload1);
                 PostDAO.AddPost(newPost);
 
                 rptUserPosts.DataSource = PostDAO.GetPostsByCircle("gym");
@@ -67,42 +66,9 @@ namespace MyCircles.Home
             }
         }
 
-        protected string UploadThisFile(FileUpload upload)
-        {
-            if (upload.HasFile)
-            {
-                string filename = currentUser.Id + '-' + DateTime.Now.ToString("yyyyMMdd_hhmmss") + '-' + upload.FileName;
-                string filepath = Server.MapPath(Path.Combine("/Content/images/shared/" + filename));
-                upload.SaveAs(filepath);
-
-                return "/Content/images/shared/" + filename;
-            }
-
-            return null;
-        }
-
         protected void Btncircle_Click(object sender, EventArgs e)
         {
           
            
         }
-        protected void UploadFile(object sender, EventArgs e)
-        {
-            string folderPath = Server.MapPath("~/Files/");
-
-            //Check whether Directory (Folder) exists.
-            if (!Directory.Exists(folderPath))
-            {
-                //If Directory (Folder) does not exists Create it.
-                Directory.CreateDirectory(folderPath);
-            }
-
-            //Save the File to the Directory (Folder).
-            //upldFile.SaveAs(folderPath + Path.GetFileName(upldFile.FileName));
-
-            //Display the Picture in Image control.
-           // Image1.ImageUrl = "~/Files/" + Path.GetFileName(upldFile.FileName);
-        }
-
-    }
 }
